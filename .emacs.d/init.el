615d0b614fd1b711173c9e0d74d68b230601816e
thorinf <thorin.j.a.farnsworth@gmail.com>
2024-12-09 23:18:04 +0000

    Evil-mode Orgmode setup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,15 +14,33 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; evil mode
+;; evil mode (vim emulation)
 (use-package evil
   :config
   (evil-mode 1))
 
-;; org mode
+;; org mode configuration
 (use-package org
   :ensure t
   :config
   (setq org-startup-indented t)
   (setq org-hide-leading-stars t)
   (setq org-ellipsis " ⬎"))
+
+;; evil-org integration for better navigation
+(use-package evil-org
+  :ensure t
+  :after org
+  :hook (org-mode . evil-org-mode)
+  :config
+  (evil-org-set-key-theme '(navigation insert textobjects additional calendar)))
+
+;; enable org-indent-mode for cleaner structure visualization
+(add-hook 'org-mode-hook #'org-indent-mode)
+
+(custom-set-variables '(package-selected-packages '(evil-org org-roam evil)))
+(custom-set-faces)
+
+(with-eval-after-load 'org
+  (evil-define-key 'normal org-mode-map (kbd "TAB") 'org-cycle))
+(evil-define-key 'normal org-mode-map (kbd "TAB") 'org-cycle))
