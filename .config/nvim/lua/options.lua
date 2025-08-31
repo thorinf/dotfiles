0c42559e111ceaf44edbb6aa9d554ceca784e356
thorinf <thorin.j.a.farnsworth@gmail.com>
2025-08-31 19:15:19 +0100

    feat(nvim): set tokyonight theme; force transparent background

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,5 +2,13 @@ require "nvchad.options"
 
 -- add yours here!
 
+-- Force transparent background
+vim.cmd([[
+  highlight Normal guibg=NONE ctermbg=NONE
+  highlight NormalNC guibg=NONE ctermbg=NONE
+  highlight SignColumn guibg=NONE ctermbg=NONE
+  highlight EndOfBuffer guibg=NONE ctermbg=NONE
+]])
+
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
