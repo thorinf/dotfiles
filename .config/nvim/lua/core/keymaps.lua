8845891fe9c88711f48173e0aa3b3983d99273d7
thorinf <thorin.j.a.farnsworth@gmail.com>
2025-10-09 10:57:07 +0100

    feat(nvim): add zen diagnostics toggle
    Signed-off-by: thorinf <thorin.j.a.farnsworth@gmail.com>

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,2 +1,27 @@
 -- accessing built-in netrw, as a backup
 vim.keymap.set("n", "<leader>E", "<cmd>Explore<CR>", { desc = "Open netrw (backup)" })
+
+-- zen mode toggle - hide warnings
+vim.keymap.set("n", "<leader>z", function()
+  local zen_enabled = vim.g.zen_mode_enabled or false
+
+  if zen_enabled then
+    -- Restore diagnostics
+    vim.diagnostic.config({
+      virtual_text = true,
+      underline = true,
+    })
+    vim.g.zen_mode_enabled = false
+  else
+    -- Disable warnings virtual text and underline
+    vim.diagnostic.config({
+      virtual_text = {
+        severity = { min = vim.diagnostic.severity.ERROR },
+      },
+      underline = {
+        severity = { min = vim.diagnostic.severity.ERROR },
+      },
+    })
+    vim.g.zen_mode_enabled = true
+  end
+end, { desc = "Toggle zen mode" })
