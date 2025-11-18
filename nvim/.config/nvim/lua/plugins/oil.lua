491d6decf21314f1ddb37f957871eed5c13a4518
thorinf <thorin.j.a.farnsworth@gmail.com>
2025-11-18 12:20:45 +0000

    feat(oil): added current dir bar to oil buffer

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,3 +1,14 @@
+function _G.get_oil_winbar()
+  -- statusline_winid exists only while the winbar/statusline is evaluated; fall back to the active win
+  local winid = vim.g.statusline_winid or vim.api.nvim_get_current_win()
+  local bufnr = vim.api.nvim_win_get_buf(winid)
+  local dir = require("oil").get_current_dir(bufnr)
+  if dir then
+    return vim.fn.fnamemodify(dir, ":~")
+  end
+  return vim.api.nvim_buf_get_name(bufnr)
+end
+
 return {
   {
     "stevearc/oil.nvim",
@@ -5,6 +16,9 @@ return {
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     opts = {
       default_file_explorer = false,
+      win_options = {
+        winbar = "%!v:lua.get_oil_winbar()",
+      },
     },
     keys = {
       { "-", "<cmd>Oil<CR>", desc = "Open parent directory" },
