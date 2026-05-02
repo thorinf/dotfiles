bd21b5ad6819af2c8919b6100472a7dd297ea8b1
thorinf <thorin.j.a.farnsworth@gmail.com>
2026-05-03 00:00:53 +0100

    chore(emacs): remove unused org-roam from package-selected-packages

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -49,7 +49,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil-org org-roam evil)))
+ '(package-selected-packages '(evil-org evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
