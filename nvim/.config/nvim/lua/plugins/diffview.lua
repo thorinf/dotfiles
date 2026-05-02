8f05e44295be6ac8cdb66a323fa0bfb8c2227527
thorinf <thorin.j.a.farnsworth@gmail.com>
2026-05-02 18:17:50 +0100

    feat(nvim): floating diffview file panel and cleaner defaults

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -2,11 +2,31 @@ return {
   "sindrets/diffview.nvim",
   cmd = { "DiffviewOpen", "DiffviewFileHistory" },
   keys = {
-    { "<leader>gf", ":DiffviewOpen origin/main...HEAD<CR>", desc = "Diff vs main" },
+    { "<leader>gf", ":DiffviewOpen origin/main<CR>", desc = "Diff vs main" },
     { "<leader>gq", ":DiffviewClose<CR>", desc = "Close diff" },
+    { "<leader>gl", ":DiffviewToggleFiles<CR>", desc = "Toggle diffview file list" },
   },
   opts = {
     enhanced_diff_hl = true,
-    file_panel = { listing_style = "list" },
+    file_panel = {
+      listing_style = "tree",
+      tree_options = { flatten_dirs = true, folder_statuses = "only_folded" },
+      win_config = {
+        type = "float",
+        position = "center",
+        relative = "editor",
+        width = 60,
+        height = 25,
+        border = "rounded",
+      },
+    },
+    hooks = {
+      view_opened = function()
+        vim.cmd("DiffviewToggleFiles")
+      end,
+      diff_buf_win_enter = function(_, win)
+        vim.wo[win].winbar = ""
+      end,
+    },
   },
 }
